3a570ae84ce2a8bd134f673f70614ab5ba5cdcde
zay0lol <zynahisthebest2012@gmail.com>
2024-12-28 21:46:45 +0500

    dts

---
+++ main.py
@@ -0,0 +1,31 @@
+mylist=["cheese","milk","banana","meat"]
+print(mylist[0])
+mylist.append("heavy cream")
+mylist.pop(3)
+print(mylist)
+mylist.sort()
+print(mylist)
+mylist.reverse()
+print(mylist)
+mylist.clear
+print(mylist)
+len(mylist)
+print(len(mylist))
+print(mylist[-1])
+mylist.append("eggs")
+print(mylist)
+a = mylist
+
+res = ' '.join(a)
+
+print(res)
+
+my_dict={'name':'zynah' , 'age':12 , 'class':7 , 'gender':'female'}
+my_dict['name']= "zain"
+print(my_dict['name'])
+my_dict['gender']= 'male'
+print(my_dict)
+my_dict.pop('age','class')
+print(my_dict)
+my_dict.clear()
+print(my_dict)
